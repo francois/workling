3c823c987da47be72b20fa365546c2296a706c92
François Beausoleil <francois@teksol.info>
2008-06-02 01:30:48 -0400

    Implemented Workling::Base#logger.

--- lib/workling/base.rb
+++ lib/workling/base.rb
@@ -4,6 +4,10 @@ module Workling
       Workling::Discovery.discovered << subclass
     end
     
+    def logger
+      RAILS_DEFAULT_LOGGER
+    end
+    
     # thanks to blaine cook for this suggestion.
     def self.method_missing(method, *args, &block)
       if method.to_s =~ /^asynch_(.*)/
